abf7ffe035168628f312ab4716d0e52a84f59b64
DareAnanas <nylo2005@gmail.com>
2024-01-02 12:49:03 +0200

    Completed It should manage a more complex family tree (open recursion)

--- JSProblems/Teknonymy/script.js
+++ JSProblems/Teknonymy/script.js
@@ -1,21 +1,3 @@
-// function breadthFirstTraversal(root) {
-//     if (!root) {
-//         return [];
-//     }
-
-//     let result = [];
-//     let queue = [root];
-
-//     while (queue.length > 0) {
-//         let currentNode = queue.shift();
-//         console.log(currentNode);
-//         result.push(currentNode.name);
-//         queue.push(...currentNode.children);
-//     }
-
-//     return result;
-// }
-
 function isLeaf(node) {
     if (node.children.length == 0)
         return true;
@@ -32,28 +14,113 @@ function findElder(children) {
 }
 
 function getTeknonym(sex, elder) {
+    let parentSex = '';
     switch (sex) {
         case 'm':
-            return `father of ${elder.name}`;
+            parentSex = 'father';
+            break;
         case 'f':
-            return `mother of ${elder.name}`;
+            parentSex = 'mother';
+            break;
+    }
+    return `${parentSex} of ${elder.name};${elder.dateOfBirth}`;
+}
+
+function ageCompareTeknonyms(a, b) {
+    a = a.slice(a.indexOf(';')+1);
+    b = b.slice(b.indexOf(';')+1);
+    return a < b;
+}
+
+function teknonymTrimDate(teknonym) {
+    let index = teknonym.indexOf(';');
+    if (index == -1) return teknonym;
+    return teknonym.slice(0, index);
+}
+
+function plusGeneration(sex, teknonym) {
+    let parentSex = '';
+    switch (sex) {
+        case 'm':
+            parentSex = 'father';
+            break;
+        case 'f':
+            parentSex = 'mother';
+            break;
+    }
+    teknonym = teknonym.replace(/father|mother/g, parentSex);
+    if (/grand/g.test(teknonym)) {
+        teknonym = 'great-' + teknonym;
+    } else {
+        teknonym = 'grand' + teknonym;
+    }
+    return teknonym;
+}
+
+function setTeknonym2(node) {
+    if (!isLeaf(node)) {
+        let allLeafs = true;
+        for (let child of node.children) {
+            if (!isLeaf(child)) {
+                allLeafs = false;
+            }
+        }
+        if (allLeafs) {
+            let elder = findElder(node.children);
+            node.teknonym = getTeknonym(node.sex, elder);
+        } else {
+            let elderTeknonym = '';
+            for (let child of node.children) {
+                setTeknonym2(child);
+                if (elderTeknonym != '' && child.teknonym != '' &&
+                ageCompareTeknonyms(child.teknonym, elderTeknonym)) {
+                    elderTeknonym = child.teknonym;
+                    child.teknonym = teknonymTrimDate(child.teknonym);
+                } else if (elderTeknonym == '' && child.teknonym != '') {
+                    elderTeknonym = child.teknonym;
+                    child.teknonym = teknonymTrimDate(child.teknonym);
+                }
+            }
+            elderTeknonym = teknonymTrimDate(elderTeknonym);
+            node.teknonym = plusGeneration(node.sex, elderTeknonym);
+        }
+    }
+}
+
+function setTeknonym1(node) {
+    if (!isLeaf(node)) {
+        let allLeafs = true;
+        for (let child of node.children) {
+            if (!isLeaf(child)) {
+                allLeafs = false;
+            }
+        }
+        if (allLeafs) {
+            let elder = findElder(node.children);
+            node.teknonym = getTeknonym(node.sex, elder);
+        } else {
+            let elderTeknonym = '';
+            for (let child of node.children) {
+                setTeknonym2(child);
+                if (elderTeknonym != '' && child.teknonym != '' &&
+                ageCompareTeknonyms(child.teknonym, elderTeknonym)) {
+                    elderTeknonym = child.teknonym;
+                    child.teknonym = teknonymTrimDate(child.teknonym);
+                } else if (elderTeknonym == '' && child.teknonym != '') {
+                    elderTeknonym = child.teknonym;
+                    child.teknonym = teknonymTrimDate(child.teknonym);
+                }
+            }
+            elderTeknonym = teknonymTrimDate(elderTeknonym);
+            node.teknonym = plusGeneration(node.sex, elderTeknonym);
+        }
     }
 }
 
 function teknonymize(familyTree){
     if (isLeaf(familyTree)) return;
-    if (!isLeaf(familyTree)) {
-        let allLeafs = true;
-        for (let node of familyTree.children) {
-            if (!isLeaf(node)) {
-                allLeafs = false;
-            }
-        }
-        if (allLeafs) {
-            let elder = findElder(familyTree.children);
-            familyTree.teknonym = getTeknonym(familyTree.sex, elder);
-        }
-    }
+    setTeknonym1(familyTree);
+    familyTree.teknonym = teknonymTrimDate(familyTree.teknonym);
 }
 
 let a = {
@@ -94,7 +161,69 @@ let b = {
     dateOfBirth: '1023-12-28T00:00:00.000Z'
 }
 
+let c = {
+    'sex': 'm',
+    'teknonym': '',
+    'children': [
+        {
+            'sex': 'f',
+            'teknonym': '',
+            'children': [
+                {
+                    'sex': 'f',
+                    'teknonym': '',
+                    'children': [],
+                    'name': 'h',
+                    'dateOfBirth': '1047-02-01T00:00:00.000Z'
+                }
+            ],
+            'name': 'b',
+            'dateOfBirth': '1020-02-01T00:00:00.000Z'
+        },
+        {
+            'sex': 'm',
+            'teknonym': '',
+            'children': [],
+            'name': 'c',
+            'dateOfBirth': '1021-03-01T00:00:00.000Z'
+        },
+        {
+            'sex': 'm',
+            'teknonym': '',
+            'children': [
+                {
+                    'sex': 'f',
+                    'teknonym': '',
+                    'children': [],
+                    'name': 'e',
+                    'dateOfBirth': '1043-12-01T00:00:00.000Z'
+                },
+                {
+                    'sex': 'f',
+                    'teknonym': '',
+                    'children': [],
+                    'name': 'f',
+                    'dateOfBirth': '1045-02-01T00:00:00.000Z'
+                },
+                {
+                    'sex': 'm',
+                    'teknonym': '',
+                    'children': [],
+                    'name': 'g',
+                    'dateOfBirth': '1046-02-01T00:00:00.000Z'
+                }
+            ],
+            'name': 'd',
+            'dateOfBirth': '1023-12-28T00:00:00.000Z'
+        }
+    ],
+    'name': 'a',
+    'dateOfBirth': '1000-02-01T00:00:00.000Z'
+}
+
 teknonymize(a);
 console.log(a);
 teknonymize(b);
 console.log(b);
+teknonymize(c);
+console.log(c);
