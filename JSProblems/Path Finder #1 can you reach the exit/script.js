224a4c0b9358e60907917f11199440e8cdbb7cdc
DareAnanas <nylo2005@gmail.com>
2024-01-07 21:04:34 +0200

    How to handle fake is returned to start ?

--- JSProblems/Path Finder #1 can you reach the exit/script.js
+++ JSProblems/Path Finder #1 can you reach the exit/script.js
@@ -1,3 +1,4 @@
+const DIRECTION_COUNT = 4;
 // cardinal directions
 const NORTH = 0;
 const EAST = 1;
@@ -27,12 +28,14 @@ class Turtle {
         this.facing = facing;
     }
 
-    #getCellValue() {
-
+    getCellValue(x, y) {
+        if (this.maze[y] == undefined || this.maze[y][x] == undefined)
+            return 'W';
+        return this.maze[y][x];
     }
     
     isInFinish() {
-        if (this.x == this.maze.length - 1 &&
+        if (this.x == this.maze[0].length - 1 &&
             this.y == this.maze.length - 1)
             return true;
         return false;
@@ -43,37 +46,107 @@ class Turtle {
             this.y == this.startingY &&
             this.hasLeftStart)
             return true;
-        if (this.x == this.startingX &&
-            this.y == this.startingY) {
-            this.hasLeftStart = true;
-            return false;
+        return false;
+    }
+
+    localToCardinal(localDirection) {
+        return (localDirection + this.facing) % DIRECTION_COUNT;
+    }
+
+    isFreeWay(localDirection) {
+        let cardinalDirection = this.localToCardinal(localDirection);
+        switch (cardinalDirection) {
+            case NORTH:
+                if (this.getCellValue(this.x, this.y - 1) == '.')
+                    return true;
+                break;
+            case EAST:
+                if (this.getCellValue(this.x + 1, this.y) == '.')
+                    return true;
+                break;
+            case SOUTH:
+                if (this.getCellValue(this.x, this.y + 1) == '.')
+                    return true;
+                break;
+            case WEST:
+                if (this.getCellValue(this.x - 1, this.y) == '.')
+                    return true;
+                break;
         }
         return false;
     }
 
-    isFreeWay(direction) {
-        switch (direction) {
-            case FORWARD:
-                
+    turnLeft() {
+        this.facing = (this.facing + 3) % DIRECTION_COUNT;
+    }
+
+    turnRight() {
+        this.facing = (this.facing + 1) % DIRECTION_COUNT;
+    }
+
+    forward() {
+        if (!this.hasLeftStart)
+            this.hasLeftStart = true;
+        let cardinalDirection = this.localToCardinal(FORWARD);
+        switch (cardinalDirection) {
+            case NORTH:
+                this.y -= 1;
                 break;
-        
-            default:
+            case EAST:
+                this.x += 1;
+                break;
+            case SOUTH:
+                this.y += 1;
+                break;
+            case WEST:
+                this.x -= 1;
                 break;
         }
     }
+}
+
+function toMatrixMaze(maze) {
+    let matrix = [];
+    for (let e of maze.split('\n')) {
+        matrix.push(e.split(''));
+    }
+    return matrix;
 }
 
 function pathFinder(maze){
+    maze = toMatrixMaze(maze);
     let turtle = new Turtle(maze, 0, 0);
-    if (turtle.isInFinish())
+    while (true) {
+        if (turtle.isInFinish())
         return true;
-    if (turtle.isReturnedToStart())
-        return false;
-
+        if (turtle.isReturnedToStart())
+            return false;
+        if (turtle.isFreeWay(LEFT))
+            turtle.turnLeft();
+        if (turtle.isFreeWay(FORWARD)) {
+            turtle.forward();
+        }
+        else
+            turtle.turnRight();
+    }
 }
 
-let maze1 = `
+let maze1 = `.W.
 .W.
-.W.
-...
-`;
+...`;
+
+let maze2 = `.W....
+WWW...
+...W.W
+.W.WWW
+.W....
+......`;
+
+let maze3 = `..W.
+.W..
+...W
+.W..`;
+
+console.log(pathFinder(maze1));
+console.log(pathFinder(maze2));
+console.log(pathFinder(maze3));
