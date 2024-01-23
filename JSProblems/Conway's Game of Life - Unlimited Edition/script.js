dc4751526db30cf37bdb52efaef2a67ad5752add
DareAnanas <nylo2005@gmail.com>
2024-01-23 13:41:26 +0200

    Switched code reading direction from bottom to top into top to bottom. Completed Game of Life - Unlimited edition

--- JSProblems/Conway's Game of Life - Unlimited Edition/script.js
+++ JSProblems/Conway's Game of Life - Unlimited Edition/script.js
@@ -1,3 +1,6 @@
+const DEAD = 0;
+const LIFE = 1;
+
 const NORTH = 0;
 const NORTH_EAST = 1;
 const EAST = 2;
@@ -25,8 +28,241 @@ const EIGHT_DIRECTIONS_ORDER = [
     NORTH_WEST
 ]
 
-const DEAD = 0;
-const LIFE = 1;
+function getGeneration(cells, generations) {
+    let lifeGrid = new LifeGrid(copyMatrix(cells));
+
+    for (let i = 0; i < generations; i++) {
+        lifeGrid.wrapWithDeadCells();
+        lifeGrid.calculateNextStates();
+        lifeGrid.nextGen();
+    }
+
+    lifeGrid.cropDeadCells();
+    return lifeGrid.cells;
+}
+
+class LifeGrid {
+    cells;
+    nextStates;
+    currentCellX;
+    currentCellY;
+    currentCellState;
+    numberOfLiveNeighbours;
+
+    constructor(cells) {
+        this.cells = cells;
+    }
+
+    wrapWithDeadCells() {
+        for (let direction of FOUR_DIRECTIONS_OREDER) {
+            if (this.checkSideForLiveCells(direction)) {
+                this.addDeadCellToSide(direction);
+            }
+        }
+    }
+
+    checkSideForLiveCells(direction) {
+        let isLive = false;
+        switch (direction) {
+            case NORTH:
+                for (let x = 0; x < this.getAxisLength('x'); x++) {
+                    if (this.getCellStateByCoordinates(x, 0) == LIFE) {
+                        isLive = true;
+                        break;
+                    }
+                }
+                break;
+            case EAST:
+                for (let y = 0; y < this.getAxisLength('y'); y++) {
+                    if (this.getCellStateByCoordinates(this.getAxisLength('x') - 1, y) == LIFE) {
+                        isLive = true;
+                        break;
+                    }
+                }
+                break;
+            case SOUTH:
+                for (let x = 0; x < this.getAxisLength('x'); x++) {
+                    if (this.getCellStateByCoordinates(x, this.getAxisLength('y') - 1) == LIFE) {
+                        isLive = true;
+                        break;
+                    }
+                }
+                break;
+            case WEST:
+                for (let y = 0; y < this.getAxisLength('y'); y++) {
+                    if (this.getCellStateByCoordinates(0, y) == LIFE) {
+                        isLive = true;
+                        break;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+        return isLive;
+    }
+
+    addDeadCellToSide(direction) {
+        let deadCells = [];
+        switch (direction) {
+            case NORTH:
+                deadCells = Array(this.getAxisLength('x')).fill(0);
+                this.cells.unshift(deadCells);
+                break;
+            case EAST:
+                for (let y = 0; y < this.getAxisLength('y'); y++) {
+                    this.cells[y].push(0);
+                }
+                break;
+            case SOUTH:
+                deadCells = Array(this.getAxisLength('x')).fill(0);
+                this.cells.push(deadCells);
+                break;
+            case WEST:
+                for (let y = 0; y < this.getAxisLength('y'); y++) {
+                    this.cells[y].unshift(0);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    calculateNextStates() {
+        this.nextStates = copyMatrix(this.cells);
+        for (let y = 0; y < this.getAxisLength('y'); y++) {
+            for (let x = 0; x < this.getAxisLength('x'); x++) {
+                this.currentCellX = x;
+                this.currentCellY = y;
+                this.currentCellState = this.getCellStateByCoordinates(x, y);
+                this.numberOfLiveNeighbours = this.getNumberOfLiveNeighbours();
+                if (this.underPopulation())
+                    this.setNextState(x, y, DEAD);
+                else if (this.overcrowding())
+                    this.setNextState(x, y, DEAD);
+                else if (this.reproduction())
+                    this.setNextState(x, y, LIFE);
+                else 
+                    this.setNextState(x, y, this.currentCellState);
+            }
+        }
+    }
+
+    getNumberOfLiveNeighbours() {
+        let count = 0;
+        for (let direction of EIGHT_DIRECTIONS_ORDER) {
+            if (this.getNeighbourState(direction) == LIFE) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    getNeighbourState(direction) {
+        let modifier = new DirectionModifier(direction);
+        return this.getCellStateByCoordinates(
+            this.currentCellX + modifier.x, 
+            this.currentCellY + modifier.y
+        );
+    }
+
+    underPopulation() {
+        if (this.isLifeCell() && this.numberOfLiveNeighbours < 2)
+            return true;
+        return false;
+    }
+
+    overcrowding() {
+        if (this.isLifeCell() && this.numberOfLiveNeighbours > 3)
+            return true;
+        return false;
+    }
+
+    reproduction() {
+        if (this.isDeadCell() && this.numberOfLiveNeighbours == 3)
+            return true;
+        return false;
+    }
+
+    isLifeCell() {
+        if (this.currentCellState == LIFE) {
+            return true;
+        }
+        return false;
+    }
+
+    isDeadCell() {
+        if (this.currentCellState == DEAD) {
+            return true;
+        }
+        return false;
+    }
+
+    setNextState(x, y, state) {
+        this.nextStates[y][x] = state;
+    }
+
+    getAxisLength(axis) {
+        if (axis == 'x')
+            return this.cells[0].length;
+        if (axis == 'y')
+            return this.cells.length;
+    }
+
+    getCellStateByCoordinates(x, y) {
+        if (this.cells[y] == undefined || this.cells[y][x] == undefined)
+            return DEAD;
+        return this.cells[y][x];
+    }
+
+    nextGen() {
+        this.cells = copyMatrix(this.nextStates);
+    }
+
+    cropDeadCells() {
+        for (let direction of FOUR_DIRECTIONS_OREDER) {
+            while (this.checkSideForAllDeadCells(direction)) {
+                this.cropSideFromDeadCells(direction);
+            }
+        } 
+    }
+
+    checkSideForAllDeadCells(direction) {
+        return !this.checkSideForLiveCells(direction);
+    }
+
+    cropSideFromDeadCells(direction) {
+        switch (direction) {
+            case NORTH:
+                this.cells.shift();
+                break;
+            case EAST:
+                for (let y = 0; y < this.getAxisLength('y'); y++) {
+                    this.cells[y].pop();
+                }
+                break;
+            case SOUTH:
+                this.cells.pop();
+                break;
+            case WEST:
+                for (let y = 0; y < this.getAxisLength('y'); y++) {
+                    this.cells[y].shift();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+ 
+}
+
+function copyMatrix(matrix) {
+    let result = [];
+    for (let y = 0; y < matrix.length; y++) {
+        result.push([...matrix[y]]);
+    }
+    return result;
+}
 
 class DirectionModifier {
     x;
@@ -70,222 +306,6 @@ class DirectionModifier {
     }
 }
 
-class Cell {
-    x;
-    y;
-    state;
-    nextState;
-
-    constructor(x, y, state) {
-        this.x = x;
-        this.y = y;
-        this.state = state;
-    }
-}
-
-class LifeGrid {
-    cells;
-    currentCell;
-    numberOfLiveNeighbours;
-
-    constructor(cells) {
-        this.cells = cells;
-    }
-
-    getCellByCoordinates(x, y) {
-        if (this.cells[y] == undefined || this.cells[y][x] == undefined)
-            return new Cell(x, y, DEAD);
-        return this.cells[y][x];
-    }
-
-    getNeighbour(direction) {
-        let modifier = new DirectionModifier(direction);
-        return this.getCellByCoordinates(
-            this.currentCell.x + modifier.x, 
-            this.currentCell.y + modifier.y
-        );
-    }
-
-    getNumberOfLiveNeighbours() {
-        let count = 0;
-        for (let direction of EIGHT_DIRECTIONS_ORDER) {
-            if (this.getNeighbour(direction).state == LIFE) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    isLifeCell() {
-        if (this.currentCell.state == LIFE) {
-            return true;
-        }
-        return false;
-    }
-
-    isDeadCell() {
-        if (this.currentCell.state == DEAD) {
-            return true;
-        }
-        return false;
-    }
-
-    underPopulation() {
-        if (this.isLifeCell() && this.numberOfLiveNeighbours < 2)
-            return true;
-        return false;
-    }
-
-    overcrowding() {
-        if (this.isLifeCell() && this.numberOfLiveNeighbours > 3)
-            return true;
-        return false;
-    }
-
-    reproduction() {
-        if (this.isDeadCell() && this.numberOfLiveNeighbours == 3)
-            return true;
-        return false;
-    }
-
-    getAxisLength(axis) {
-        if (axis == 'x')
-            return this.cells[0].length;
-        if (axis == 'y')
-            return this.cells.length;
-    }
-
-    checkSideForLiveCells(direction) {
-        let isLive = false;
-        switch (direction) {
-            case NORTH:
-                for (let x = 0; x < this.getAxisLength('x'); x++) {
-                    if (this.getCellByCoordinates(x, 0).state == LIFE) {
-                        isLive = true;
-                        break;
-                    }
-                }
-                break;
-            case EAST:
-                for (let y = 0; y < this.getAxisLength('y'); y++) {
-                    if (this.getCellByCoordinates(this.getAxisLength('x') - 1, y).state == LIFE) {
-                        isLive = true;
-                        break;
-                    }
-                }
-                break;
-            case SOUTH:
-                for (let x = 0; x < this.getAxisLength('x'); x++) {
-                    if (this.getCellByCoordinates(x, this.getAxisLength('y') - 1).state == LIFE) {
-                        isLive = true;
-                        break;
-                    }
-                }
-                break;
-            case WEST:
-                for (let y = 0; y < this.getAxisLength('y'); y++) {
-                    if (this.getCellByCoordinates(0, y).state == LIFE) {
-                        isLive = true;
-                        break;
-                    }
-                }
-                break;
-            default:
-                break;
-        }
-        return isLive;
-    }
-
-    addDeadCellToSide(direction) {
-        let deadCells = [];
-        switch (direction) {
-            case NORTH:
-                deadCells = Array(this.getAxisLength('x')).fill(0);
-                this.cells.unshift(deadCells);
-                break;
-            case EAST:
-                for (let y = 0; y < this.getAxisLength('y'); y++) {
-                    this.cells[y].push(0);
-                }
-                break;
-            case SOUTH:
-                deadCells = Array(this.getAxisLength('x')).fill(0);
-                this.cells.push(deadCells);
-                break;
-            case WEST:
-                for (let y = 0; y < this.getAxisLength('y'); y++) {
-                    this.cells[y].unshift(0);
-                }
-                break;
-            default:
-                break;
-        }
-    }
-
-    wrapWithDeadCells() {
-
-    }
-
-    calculateNextStates() {
-        for (let y = 0; y < this.getAxisLength('y'); y++) {
-            for (let x = 0; x < this.getAxisLength('x'); x++) {
-                this.currentCell = this.getCellByCoordinates(x, y);
-                this.numberOfLiveNeighbours = this.getNumberOfLiveNeighbours();
-                if (this.underPopulation())
-                    this.currentCell.nextState = DEAD;
-                else if (this.overcrowding())
-                    this.currentCell.nextState = DEAD;
-                else if (this.reproduction())
-                    this.currentCell.nextState = LIFE;
-                else 
-                    this.currentCell.nextState = this.currentCell.state;
-            }
-        }
-    }
-
-    nextGen() {
-        for (let y = 0; y < this.getAxisLength('y'); y++) {
-            for (let x = 0; x < this.getAxisLength('x'); x++) {
-                this.currentCell = this.getCellByCoordinates(x, y);
-                this.currentCell.state = this.currentCell.nextState;
-            }
-        }
-    }
-}
-
-function transformIntoClassCells(cells) {
-    for (let y = 0; y < cells.length; y++) {
-        for (let x = 0; x < cells[0].length; x++) {
-            cells[y][x] = new Cell(x, y, cells[y][x]);
-        }
-    }
-    return cells;
-}
-
-function transformFromClassCells(cells) {
-    for (let y = 0; y < cells.length; y++) {
-        for (let x = 0; x < cells[0].length; x++) {
-            cells[y][x] = cells[y][x].state;
-        }
-    }
-    return cells;
-}
-
-function nextGen(cells) {
-    cells = transformIntoClassCells(cells);
-
-    let lifeGrid = new LifeGrid(cells);
-
-    lifeGrid.addDeadCellToSide(NORTH);
-    console.log(lifeGrid.cells);
-
-    lifeGrid.calculateNextStates();
-    lifeGrid.nextGen();
-
-    cells = transformFromClassCells(lifeGrid.cells);
-    return cells;
-}
-
 let blinker = [
     [ 0, 1, 0 ], 
     [ 0, 1, 0 ], 
@@ -318,7 +338,11 @@ let pulsar = [
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 ]
 
-console.log(nextGen(blinker));
-console.log(nextGen(glider));
-console.log(nextGen(pulsar));
-  
+// getGeneration(blinker);
+// getGeneration(glider);
+// getGeneration(pulsar);
+
+console.log(getGeneration(blinker));
+console.log(getGeneration(glider));
+console.log(getGeneration(pulsar));
+
