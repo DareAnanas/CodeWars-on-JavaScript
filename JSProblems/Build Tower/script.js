e39dea960cd6cc773790999b5eac3d0909d24692
DareAnanas <nylo2005@gmail.com>
2024-03-29 18:50:27 +0200

    Completed Build Tower Added chai template Added Build Tower Advanced

---
+++ JSProblems/Build Tower/script.js
@@ -0,0 +1,27 @@
+function buildTower(n) {
+    const towerWidth = 2*n-1;
+    let tower = [];
+    for (let i = 1; i <= towerWidth; i+=2) {
+        const spaceWidth = (towerWidth - i) / 2;
+        let layer = ' '.repeat(spaceWidth) +
+                    '*'.repeat(i) +
+                    ' '.repeat(spaceWidth);
+        tower.push(layer);
+    }
+    return tower;
+}
+
+function chaiTest() {
+    let assert = chai.assert;
+
+    assert.deepEqual(buildTower(1), ['*']);
+    assert.deepEqual(buildTower(2), [' * ', '***']);
+    assert.deepEqual(buildTower(3), ['  *  ', ' *** ', '*****']);
+}
+
+function main() {
+    chaiTest();
+    buildTower(1);
+}
+
+main();
