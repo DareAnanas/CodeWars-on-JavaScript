202249790c478656b830cc3bea45cffbd01c873f
DareAnanas <nylo2005@gmail.com>
2024-03-30 10:05:39 +0200

    Completed Xshape

--- JSProblems/ASCII Fun #1 X-Shape/script.js
+++ JSProblems/ASCII Fun #1 X-Shape/script.js
@@ -3,13 +3,17 @@ function x(n) {
     let mainDiagonalIndex = 0;
     let addDiagonalIndex = n - 1;
     for (let i = 0; i < n; i++) {
-        let layer = '□'.repeat(n);
+        let layer = new Array(n);
+        for (let j = 0; j < n; j++) {
+            layer[j] = '□';
+        }
         layer[mainDiagonalIndex] = '■';
         layer[addDiagonalIndex] = '■';
-        xShape += layer + '\n';
+        xShape += layer.join('') + '\n';
         mainDiagonalIndex++;
         addDiagonalIndex--;
     }
+    xShape = xShape.slice(0, -1);
     return xShape;
 }
 
@@ -19,7 +23,7 @@ function chaiTest() {
 
 function main() {
     chaiTest();
-    console.log(x(3));
+    console.log(x(5));
 }
 
 main();
